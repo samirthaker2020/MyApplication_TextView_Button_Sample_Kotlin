9e48487ddef8f964738b202898f038c94a463052
samirthaker2020 <46947688+samirthaker2020@users.noreply.github.com>
2021-01-18 00:00:37 -0500

    added textview and button , implemented button click listner Run the Application

--- app/src/main/java/com/example/myapplication_textview_button_sample_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/myapplication_textview_button_sample_kotlin/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.myapplication_textview_button_sample_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // fetching ids for button and textview
+        val btn_click= findViewById<Button>(R.id.btn_click)
+        val result= findViewById<TextView>(R.id.textView_result)
+
+        btn_click.setOnClickListener()
+        {
+            result.setText("You click on the button")
+        }
     }
 }
